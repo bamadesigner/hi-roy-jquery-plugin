c5b0e3199d7a270dd4664d938ef8339ca9bf11b3
Rachel Cherry <bamadesigner@gmail.com>
2019-12-27 17:17:01 -0600

    Updating/optimizing npm packages, removing SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,49 @@
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-const gulp = require('gulp');
-const mergeMediaQueries = require('gulp-merge-media-queries');
-const minify = require('gulp-minify');
-const rename = require('gulp-rename');
-const sass = require('gulp-sass');
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const gulp = require("gulp");
+const minify = require("gulp-minify");
+const rename = require("gulp-rename");
 
-gulp.task('sass', function() {
-	gulp.src('hi-roy.scss')
-		.pipe(sass({
-			outputStyle: 'expanded'
-		}).on('error', sass.logError))
-		.pipe(mergeMediaQueries())
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(cleanCSS({
-			compatibility: 'ie8'
-		}))
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest(''))
+gulp.task("css", function(done) {
+  gulp
+    .src("hi-roy.css")
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
+    .pipe(cleanCSS())
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(gulp.dest("./"))
+    .on("end", done);
 });
 
-gulp.task('compress', function() {
-	gulp.src('hi-roy.js')
-		.pipe(minify({
-			mangle: false,
-			ext:{
-				min:'.min.js'
-			}
-		}))
-		.pipe(gulp.dest(''))
+gulp.task("js", function(done) {
+  gulp
+    .src("hi-roy.js")
+    .pipe(
+      minify({
+        mangle: false,
+        ext: {
+          min: ".min.js"
+        }
+      })
+    )
+    .pipe(gulp.dest("./"))
+    .on("end", done);
 });
 
-gulp.task('watch',function() {
-	gulp.watch(['hi-roy.scss'],['sass']);
-	gulp.watch(['hi-roy.js'],['compress']);
-});
+gulp.task("default", gulp.series("css", "js"));
 
-gulp.task('default', ['sass','compress']);
+gulp.task(
+  "watch",
+  gulp.series("default", function(done) {
+    gulp.watch(["hi-roy.css"], gulp.series("css"));
+    gulp.watch(["hi-roy.js"], gulp.series("js"));
+    return done();
+  })
+);
